0e626e5d24b1154493eb4c13efe10ad0640b4747
jiteshm17 <jiteshmalipeddi92@gmail.com>
2019-12-05 23:38:35 +0530

    added pagination for level 1

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 
 const router = express.Router();
 
+const LocalStorage = require('localStorage');
+
 const links = require('../controllers/linksController');
 
 router.get('/', links.getWords);
 
 router.get('/search', links.searchWord);
 
-router.post('/results', links.results);
+router.post('/results/:page', links.results);
+
+router.get('/results/:page', links.results);
 
 
 module.exports = router;
